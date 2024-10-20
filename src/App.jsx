090b59402419a7057cd1a6bcd558cfc044b378a0
Mauriciohnf45 <mh173509@gmail.com>
2024-10-19 22:28:52 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ImagemFavorita from './components/ImagemFavorita'; // Importando o novo componente
+
+function App() {
+  return (
+    <div>
+      <h2>Minha Imagem Favorita</h2>
+      <ImagemFavorita /> {/* Incluindo o componente ImagemFavorita */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ImagemFavorita.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function ImagemFavorita() {
+  return (
+    <img 
+      src="https://logodetimes.com/times/corinthians/logo-do-corinthians-1536.png" 
+      alt="Logo do Corinthians" 
+      style={{ width: '400px', height: 'auto', borderRadius: '8px' }}
+    />
+  );
+}
+
+export default ImagemFavorita;
